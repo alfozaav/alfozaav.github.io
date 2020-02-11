e10e1c17c03ee398201a404b36d6492296db5c95
GerZav <g.a.zavala.g@gmail.com>
2020-02-11 16:48:51 -0600

    Responsive

---
+++ Asterdots/js/app.js
@@ -0,0 +1,10 @@
+// Variable 
+const form = document.getElementById('form')
+
+// Event Listeners
+form.addEventListener('submit', sendForm)
+
+// Functions
+function sendForm(e) {
+    e.preventDefault()
+}
