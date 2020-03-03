07c9c5c75cb295fc6b4bb6eb0e1ef04684e83049
GerZav <g.a.zavala.g@gmail.com>
2020-03-02 19:01:46 -0600

    hamburger menu added

--- Asterdots/js/app.js
+++ Asterdots/js/app.js
@@ -1,10 +1,26 @@
 // Variable 
 const form = document.getElementById('form')
+let valor = false;
+
 
 // Event Listeners
 form.addEventListener('submit', sendForm)
 
+
 // Functions
+
 function sendForm(e) {
     e.preventDefault()
 }
+function hide() {
+    if (valor == false) {
+        document.getElementById('ham-menu').style.visibility = 'visible';
+        valor = true;
+    }
+    else {
+        document.getElementById('ham-menu').style.visibility = 'hidden';
+        valor = false
+    }
+}
+    
+
